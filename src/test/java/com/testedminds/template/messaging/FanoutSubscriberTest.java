a69a6caef31723ca4797f50dfa64624283e1f13c
Sarah Aslanifar <Sarah.Aslanifar@morningstar.com>
2016-04-06 16:28:02 -0500

    rabbitmq - fanout test

--- src/test/java/com/testedminds/template/messaging/FanoutSubscriberTest.java
+++ src/test/java/com/testedminds/template/messaging/FanoutSubscriberTest.java
@@ -4,7 +4,6 @@ import com.testedminds.template.config.Environment;
 import com.testedminds.template.handlers.LoggingHandler;
 import com.testedminds.template.handlers.MessageHandler;
 import org.junit.Test;
-import org.slf4j.Logger;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -16,45 +15,13 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
 
-import static com.testedminds.template.handlers.HandlingResult.failure;
 import static com.testedminds.template.handlers.HandlingResult.success;
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.*;
 
-public class DirectSubscriberTest {
-  private String MESSAGE = "This is a test.";
-
-  @Test(expected = RuntimeException.class)
-  public void shouldHandleIncorrectAmqpUrl(){
-    new Subscriber(new LoggingHandler(), "baduser:", "", "", "");
-  }
-
-  @Test
-  public void shouldNotPrintIfDebugFlagNotEnabled() throws Exception {
-    Map<String, String> env = new HashMap<>();
-    env.put("DEBUG_MESSAGE_HANDLING", "false");
-    com.testedminds.template.utils.Environment.set(env);
-
-    final Subscriber subscriber = new Subscriber(null, Environment.amqpUri(), "profile", "direct", "update");
-    Logger mockLogger = mock(Logger.class);
-    subscriber.setLogger(mockLogger);
-    subscriber.log(failure("test failure", "message"));
-    verify(mockLogger, never()).warn("test failure");
-  }
-
-  @Test
-  public void shouldPrintFailureIfDebugFlagEnabled() throws Exception {
-    Map<String, String> env = new HashMap<>();
-    env.put("DEBUG_MESSAGE_HANDLING", "true");
-    com.testedminds.template.utils.Environment.set(env);
-
-    final Subscriber sub = new Subscriber(null, Environment.amqpUri(), "profile", "direct", "update");
-    Logger mockLogger = mock(Logger.class);
-    sub.setLogger(mockLogger);
-    sub.log(failure("test failure", "message"));
-    verify(mockLogger).warn("test failure: " + "message");
-  }
-
+public class FanoutSubscriberTest {
+  private final static String MESSAGE = "This is a fanout test.";
+  private final static String EXCHANGE_NAME = "direct_profile";
+  private final static String EXCHANGE_TYPE = "fanout";
   @Test
   public void shouldProcessesMessagesWithMultipleThreads() throws Exception {
     Map<String, String> env = new HashMap<>();
@@ -67,7 +34,7 @@ public class DirectSubscriberTest {
 
     MessageHandler successHandler = successHandler(message, counter);
     // This must be declared before the publisher in order to set up state in Rabbit.
-    final Subscriber sub = new Subscriber(successHandler, Environment.amqpUri(), "profile", "direct", "update");
+    final Subscriber sub = new Subscriber(successHandler, Environment.amqpUri(), EXCHANGE_NAME, EXCHANGE_TYPE, "");
     final Publisher messenger = messenger(sub);
 
     // publish messages to the consumer we just declared.
@@ -95,11 +62,13 @@ public class DirectSubscriberTest {
   }
 
   private Publisher messenger(final Subscriber sub) {
-    final Publisher messenger = new Publisher("profile", "update", Environment.AMQP_LOCALHOST);
+    final Publisher messenger = new Publisher(EXCHANGE_NAME, "", Environment.AMQP_LOCALHOST);
     // Empty the queue from previous test runs
-    messenger.purge();
+    messenger.publish(MESSAGE);
 
     new Thread(sub::deliveryLoop).start();
     return messenger;
   }
+
+
 }
