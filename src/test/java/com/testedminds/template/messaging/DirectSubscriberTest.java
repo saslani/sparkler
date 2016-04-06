a329b15759c1244302ddb83565fb5f5628c0ac06
Sarah Aslanifar <Sarah.Aslanifar@morningstar.com>
2016-04-06 16:30:37 -0500

    rabbitmq - refactoring rests

--- src/test/java/com/testedminds/template/messaging/DirectSubscriberTest.java
+++ src/test/java/com/testedminds/template/messaging/DirectSubscriberTest.java
@@ -1,10 +1,8 @@
 package com.testedminds.template.messaging;
 
 import com.testedminds.template.config.Environment;
-import com.testedminds.template.handlers.LoggingHandler;
 import com.testedminds.template.handlers.MessageHandler;
 import org.junit.Test;
-import org.slf4j.Logger;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -16,44 +14,11 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
 
-import static com.testedminds.template.handlers.HandlingResult.failure;
 import static com.testedminds.template.handlers.HandlingResult.success;
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.*;
 
 public class DirectSubscriberTest {
   private String MESSAGE = "This is a test.";
-
-  @Test(expected = RuntimeException.class)
-  public void shouldHandleIncorrectAmqpUrl(){
-    new Subscriber(new LoggingHandler(), "baduser:", "", "", "");
-  }
-
-  @Test
-  public void shouldNotPrintIfDebugFlagNotEnabled() throws Exception {
-    Map<String, String> env = new HashMap<>();
-    env.put("DEBUG_MESSAGE_HANDLING", "false");
-    com.testedminds.template.utils.Environment.set(env);
-
-    final Subscriber subscriber = new Subscriber(null, Environment.amqpUri(), "profile", "direct", "update");
-    Logger mockLogger = mock(Logger.class);
-    subscriber.setLogger(mockLogger);
-    subscriber.log(failure("test failure", "message"));
-    verify(mockLogger, never()).warn("test failure");
-  }
-
-  @Test
-  public void shouldPrintFailureIfDebugFlagEnabled() throws Exception {
-    Map<String, String> env = new HashMap<>();
-    env.put("DEBUG_MESSAGE_HANDLING", "true");
-    com.testedminds.template.utils.Environment.set(env);
-
-    final Subscriber sub = new Subscriber(null, Environment.amqpUri(), "profile", "direct", "update");
-    Logger mockLogger = mock(Logger.class);
-    sub.setLogger(mockLogger);
-    sub.log(failure("test failure", "message"));
-    verify(mockLogger).warn("test failure: " + "message");
-  }
 
   @Test
   public void shouldProcessesMessagesWithMultipleThreads() throws Exception {
